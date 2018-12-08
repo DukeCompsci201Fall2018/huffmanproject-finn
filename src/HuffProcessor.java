9003435c44d5b3a7f7def1f084482712983d2624
Sergio Wallace-Vera <sergio.wallace.vera@duke.edu>
2018-12-07 22:53:46 -0500

    decompress

--- src/HuffProcessor.java
+++ src/HuffProcessor.java
@@ -8,7 +8,7 @@
  * and including debug and bits read/written information
  * 
  * @author Owen Astrachan
- * Names: Anshul Shah and Sergio Vera-Wallace
+ * Names: Anshul Shah and Sergio Wallace-Vera
  * netID: as817 and stw21
  */
 
@@ -64,12 +64,13 @@ public class HuffProcessor {
 			throw new HuffException("illegal header starts with "+ bits);
 		}
 		HuffNode root = readTree(in);
-		//readCompressBits(root,in,out);
+		readCompressBits(root,in,out);
+		/*
 		while (true){
 			int val = in.readBits(BITS_PER_WORD);
 			if (val == -1) break;
 			out.writeBits(BITS_PER_WORD, val);
-		}
+		}*/
 		out.close();
 	}
 
@@ -87,4 +88,28 @@ public class HuffProcessor {
 			return new HuffNode(in.readBits(BITS_PER_WORD + 1), 0, null, null);
 		}
 	}
+
+	private void readCompressBits(HuffNode root, BitInputStream in, BitOutputStream out) {
+		HuffNode current = root;
+		while (true) {
+			int bits = in.readBits(1);
+			if (bits == -1) {
+				throw new HuffException("bad input, no PSEUDO_EOF");
+			}
+			else {
+				if (bits == 0) current = current.myLeft;
+				else current = current.myRight;
+				
+				if(current.myLeft == null && current.myRight == null) {
+					if (current.myValue == PSEUDO_EOF) 
+						break;
+					else {
+						out.writeBits(BITS_PER_WORD, current.myValue);
+						current = root;
+					}
+				}
+			}
+		}
+	}
+
 }
